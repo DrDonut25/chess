61a659473b934910472dc59586d468b5a6210907
Parker Remund <remundparker@gmail.com>
2025-01-17 19:03:37 -0700

    added getters/setters for BishopMoves test, created PieceMovesCalculator class with non-implemented constructor

---
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -0,0 +1,9 @@
+package chess;
+
+import java.util.Collection;
+
+public class PieceMovesCalculator {
+    Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
+        throw new RuntimeException("not implemented");
+    }
+}
