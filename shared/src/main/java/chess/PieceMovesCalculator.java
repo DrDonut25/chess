b0de64b4a2c5ef226485e2c6f1f40696eb1d6439
Parker Remund <remundparker@gmail.com>
2025-01-18 13:27:24 -0700

    added Chess Piece fields and pieceMovesCalculator method call

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -3,6 +3,8 @@ package chess;
 import java.util.Collection;
 
 public class PieceMovesCalculator {
+    public PieceMovesCalculator() {}
+
     Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
         throw new RuntimeException("not implemented");
     }
