fb317ba2972edb4d300998897c97121d782b1ddc
DrDonut25 <remundparker@gmail.com>
2025-01-22 19:58:09 -0700

    Fully implemented BishopMovesCalculator and RookMovesCalculator

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -7,8 +7,85 @@ public class BishopMovesCalculator implements pieceMovesCalculator {
 
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
+        ChessPiece bishop = board.getPiece(position);
         ArrayList<ChessMove> moves = new ArrayList<>();
         //check diagonals --> row/col can only be incremented/decremented at the same time
+        //up-right diagonal
+        int i = 1;
+        ChessPosition endPos = new ChessPosition(position.getRow() + i, position.getColumn() + i);
+        while (endPos.isValidPosition()) {
+            //empty: add ChessMove & continue loop; occupied: break loop, but add ChessMove if piece belongs to other team
+            if (board.getPiece(endPos) == null) {
+                ChessMove move = new ChessMove(position, endPos);
+                moves.add(move);
+            } else if (board.getPiece(endPos).getTeamColor() != bishop.getTeamColor()) {
+                ChessMove move = new ChessMove(position, endPos);
+                moves.add(move);
+                break;
+            } else {
+                break;
+            }
+            i++;
+            endPos = new ChessPosition(position.getRow() + i, position.getColumn() + i);
+        }
+        //down-left diagonal
+        i = -1;
+        endPos = new ChessPosition(position.getRow() + i, position.getColumn() + i);
+        while (endPos.isValidPosition()) {
+            //empty space: add ChessMove & continue loop; occupied: break loop, but add ChessMove if piece belongs to other team
+            if (board.getPiece(endPos) == null) {
+                ChessMove move = new ChessMove(position, endPos);
+                moves.add(move);
+            } else if (board.getPiece(endPos).getTeamColor() != bishop.getTeamColor()) {
+                ChessMove move = new ChessMove(position, endPos);
+                moves.add(move);
+                break;
+            } else {
+                break;
+            }
+            i--;
+            endPos = new ChessPosition(position.getRow() + i, position.getColumn() + i);
+        }
+        //up-left diagonal
+        i = 1;
+        int j = -1;
+        endPos = new ChessPosition(position.getRow() + i, position.getColumn() + j);
+        while (endPos.isValidPosition()) {
+            //empty space: add ChessMove & continue loop; occupied: break loop, but add ChessMove if piece belongs to other team
+            if (board.getPiece(endPos) == null) {
+                ChessMove move = new ChessMove(position, endPos);
+                moves.add(move);
+            } else if (board.getPiece(endPos).getTeamColor() != bishop.getTeamColor()) {
+                ChessMove move = new ChessMove(position, endPos);
+                moves.add(move);
+                break;
+            } else {
+                break;
+            }
+            i++;
+            j--;
+            endPos = new ChessPosition(position.getRow() + i, position.getColumn() + j);
+        }
+        //down-right diagonal
+        i = -1;
+        j = 1;
+        endPos = new ChessPosition(position.getRow() + i, position.getColumn() + j);
+        while (endPos.isValidPosition()) {
+            //empty space: add ChessMove & continue loop; occupied: break loop, but add ChessMove if piece belongs to other team
+            if (board.getPiece(endPos) == null) {
+                ChessMove move = new ChessMove(position, endPos);
+                moves.add(move);
+            } else if (board.getPiece(endPos).getTeamColor() != bishop.getTeamColor()) {
+                ChessMove move = new ChessMove(position, endPos);
+                moves.add(move);
+                break;
+            } else {
+                break;
+            }
+            i--;
+            j++;
+            endPos = new ChessPosition(position.getRow() + i, position.getColumn() + j);
+        }
         return moves;
     }
 }
--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -1,11 +1,86 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 public class RookMovesCalculator implements pieceMovesCalculator{
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
-        return List.of();
+        ChessPiece rook = board.getPiece(position);
+        ArrayList<ChessMove> moves = new ArrayList<>();
+        //rook can only move horizontally/vertically --> can only increment/decrement row or column, but never both at the same time
+        //right horizontal
+        int i = 1;
+        ChessPosition endPos = new ChessPosition(position.getRow(), position.getColumn() + i);
+        while (endPos.isValidPosition()) {
+            //empty: add ChessMove & continue loop; occupied: break loop, but add ChessMove if piece belongs to other team
+            if (board.getPiece(endPos) == null) {
+                ChessMove move = new ChessMove(position, endPos);
+                moves.add(move);
+            } else if (board.getPiece(endPos).getTeamColor() != rook.getTeamColor()) {
+                ChessMove move = new ChessMove(position, endPos);
+                moves.add(move);
+                break;
+            } else {
+                break;
+            }
+            i++;
+            endPos = new ChessPosition(position.getRow(), position.getColumn() + i);
+        }
+        //left horizontal
+        i = -1;
+        endPos = new ChessPosition(position.getRow(), position.getColumn() + i);
+        while (endPos.isValidPosition()) {
+            //empty: add ChessMove & continue loop; occupied: break loop, but add ChessMove if piece belongs to other team
+            if (board.getPiece(endPos) == null) {
+                ChessMove move = new ChessMove(position, endPos);
+                moves.add(move);
+            } else if (board.getPiece(endPos).getTeamColor() != rook.getTeamColor()) {
+                ChessMove move = new ChessMove(position, endPos);
+                moves.add(move);
+                break;
+            } else {
+                break;
+            }
+            i--;
+            endPos = new ChessPosition(position.getRow(), position.getColumn() + i);
+        }
+        //up horizontal
+        i = 1;
+        endPos = new ChessPosition(position.getRow() + i, position.getColumn());
+        while (endPos.isValidPosition()) {
+            //empty: add ChessMove & continue loop; occupied: break loop, but add ChessMove if piece belongs to other team
+            if (board.getPiece(endPos) == null) {
+                ChessMove move = new ChessMove(position, endPos);
+                moves.add(move);
+            } else if (board.getPiece(endPos).getTeamColor() != rook.getTeamColor()) {
+                ChessMove move = new ChessMove(position, endPos);
+                moves.add(move);
+                break;
+            } else {
+                break;
+            }
+            i++;
+            endPos = new ChessPosition(position.getRow() + i, position.getColumn());
+        }
+        //down horizontal
+        i = -1;
+        endPos = new ChessPosition(position.getRow() + i, position.getColumn());
+        while (endPos.isValidPosition()) {
+            //empty: add ChessMove & continue loop; occupied: break loop, but add ChessMove if piece belongs to other team
+            if (board.getPiece(endPos) == null) {
+                ChessMove move = new ChessMove(position, endPos);
+                moves.add(move);
+            } else if (board.getPiece(endPos).getTeamColor() != rook.getTeamColor()) {
+                ChessMove move = new ChessMove(position, endPos);
+                moves.add(move);
+                break;
+            } else {
+                break;
+            }
+            i--;
+            endPos = new ChessPosition(position.getRow() + i, position.getColumn());
+        }
+        return moves;
     }
 }
