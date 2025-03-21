cf67d460b8bbb1ff623bb3c9bda445f1a95720c2
DrDonut25 <remundparker@gmail.com>
2025-03-21 14:24:14 -0600

    Implemented run method in Repl class, added printPrompt method that prints out terminal prompt message for user input and clears screen for enhanced readability

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -2,6 +2,8 @@ package ui;
 
 import java.util.Scanner;
 
+import static ui.EscapeSequences.*;
+
 public class Repl {
     private Client client;
 
@@ -11,5 +13,21 @@ public class Repl {
 
     public void run() {
         Scanner scanner = new Scanner(System.in);
+        String result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+            try {
+                result = client.eval(line);
+                System.out.print(SET_TEXT_COLOR_BLUE + result);
+            } catch (Throwable e) {
+                System.out.println(e.toString());
+            }
+        }
+        System.out.println();
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + ERASE_SCREEN + ">>>" + SET_TEXT_COLOR_GREEN);
     }
 }
