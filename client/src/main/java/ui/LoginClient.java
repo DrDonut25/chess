a5b63e9d7371671bfdff7cda96a70e8d112b3b46
DrDonut25 <remundparker@gmail.com>
2025-03-21 22:49:41 -0600

    Added register/login/logout methods to LoginClient, and implemented eval method in LoginClient

--- client/src/main/java/ui/LoginClient.java
+++ client/src/main/java/ui/LoginClient.java
@@ -2,9 +2,12 @@ package ui;
 
 import exception.DataAccessException;
 
+import java.util.Arrays;
+
 public class LoginClient implements Client {
     private final String serverUrl;
     private final Repl repl;
+    private String authToken;
 
     public LoginClient(String serverUrl, Repl repl) {
         this.serverUrl = serverUrl;
@@ -14,10 +17,29 @@ public class LoginClient implements Client {
     @Override
     public String eval(String input) {
         try {
-
+            String[] tokens = input.toLowerCase().split(" ");
+            String cmd = (tokens.length > 0) ? tokens[0] : "help";
+            String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "register" -> register(params);
+                case "login" -> login(params);
+                case "logout" -> logout();
+                default -> help();
+            };
         } catch (DataAccessException e) {
-
+            return e.getMessage();
         }
+    }
+
+    public String register(String[] params) throws DataAccessException {
+        return "";
+    }
+
+    public String login(String[] params) throws DataAccessException {
+        return "";
+    }
+
+    public String logout() throws DataAccessException {
         return "";
     }
 
