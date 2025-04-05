511f9b602eb17aa546a5ac56423ac6c072a390af
DrDonut25 <remundparker@gmail.com>
2025-04-05 17:26:18 -0600

    Created connect/leave/resign/makeMove methods in WebSocketHandler, and added switch statement in onMessage method calling each of them (still need to figure out how to get username from authToken)

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -3,6 +3,7 @@ package server.websocket;
 import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.*;
+import websocket.commands.MakeMoveCommand;
 import websocket.commands.UserGameCommand;
 
 @WebSocket
@@ -11,12 +12,34 @@ public class WebSocketHandler {
 
     @OnWebSocketMessage
     public void onMessage(Session session, String msg) {
-        try {
-            UserGameCommand gameCommand = new Gson().fromJson(msg, UserGameCommand.class);
+        UserGameCommand gameCommand = new Gson().fromJson(msg, UserGameCommand.class);
+        String username = getUsername(gameCommand.getAuthToken());
 
-            String username = getUsername(gameCommand.getAuthToken());
-        } catch (Exception e) {
-            connections.sendMessage();
+        switch (gameCommand.getCommandType()) {
+            case CONNECT -> connect(session, username, gameCommand);
+            case MAKE_MOVE -> makeMove(session, username, (MakeMoveCommand) gameCommand);
+            case LEAVE -> leaveGame(session, username, gameCommand);
+            case RESIGN -> resign(session, username, gameCommand);
         }
+    }
+
+    public String getUsername(String authToken) {
+        return "";
+    }
+
+    public void connect(Session session, String username, UserGameCommand command) {
+
+    }
+
+    public void makeMove(Session session, String username, MakeMoveCommand command) {
+
+    }
+
+    public void leaveGame(Session session, String username, UserGameCommand command) {
+
+    }
+
+    public void resign(Session session, String username, UserGameCommand command) {
+
     }
 }
