dba312955a5569c4f4db2b874fcca0434033b0e8
DrDonut25 <remundparker@gmail.com>
2025-04-09 15:15:02 -0600

    Added first half of WebSocketHandler code to connect/makeMove/leaveGame/resign methods

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import dataaccess.AuthDAO;
 import dataaccess.GameDAO;
 import exception.DataAccessException;
+import model.GameData;
 import org.eclipse.jetty.util.IO;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.*;
@@ -46,22 +47,40 @@ public class WebSocketHandler {
         return authDAO.getAuth(authToken).username();
     }
 
-    public void connect(Session session, String username, UserGameCommand command) throws IOException {
-        connections.add(username, session);
-        String message = username + " joined the game";
-        NotificationMessage notification = new NotificationMessage(message);
-        connections.broadcast(username, notification);
+    public void connect(Session session, String username, UserGameCommand command) throws DataAccessException {
+        try {
+            connections.add(username, session);
+            String message = username + " joined the game";
+            NotificationMessage notification = new NotificationMessage(message);
+            connections.broadcast(username, notification);
+            //Add user to ChessGame - How to add observer?
+            gameDAO.updateGame(command.getGameID(), "", username);
+        } catch (IOException e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 
     public void makeMove(Session session, String username, MakeMoveCommand command) {
-
+        String pos1 = "";
+        String pos2 = "";
+        String message = String.format("%s made a move: %s to %s", username, pos1, pos2);
+        //Update ChessGame (maybe move this up to the top?)
     }
 
-    public void leaveGame(Session session, String username, UserGameCommand command) {
-
+    public void leaveGame(Session session, String username, UserGameCommand command) throws IOException {
+        connections.remove(username);
+        String message = username + " left the game";
+        NotificationMessage notification = new NotificationMessage(message);
+        connections.broadcast(username, notification);
+        //Remove user from ChessGame
     }
 
-    public void resign(Session session, String username, UserGameCommand command) {
+    public void resign(Session session, String username, UserGameCommand command) throws IOException {
+        connections.remove(username);
+        String message = username + " forfeited the game";
+        NotificationMessage notification = new NotificationMessage(message);
+        connections.broadcast(username, notification);
+        //Remove user from ChessGame and end ChessGame
 
     }
 }
